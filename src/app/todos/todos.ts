3c00384056ae618db85af95aa3013b4e5c867130
Azula <sakthibala3107@gmail.com>
2026-02-14 20:21:19 +0530

    [UPDATE] Learned the concpts of how to loop through the responses and other stuffs

--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Todoservice } from '../service/todos';
+import { Todo } from '../model/todo.type';
 
 @Component({
   selector: 'app-todos',
+  standalone: true,
   imports: [],
   templateUrl: './todos.html',
   styleUrl: './todos.scss',
 })
-export class Todos {
 
+//
+export class Todos implements OnInit {
+  // DI
+  public todoService = inject(Todoservice);
+
+  todoItems = signal<Todo[]>([]);
+
+  // useeffect
+  ngOnInit(): void {
+    const items = this.todoService.todoItems;
+
+    if (!items.length) return;
+
+    this.todoItems.set(items);
+  }
 }
